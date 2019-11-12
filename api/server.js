7d46675e650635a96a83a89e3ba3e1b05629dcae
kly001 <karenly1961@gmail.com>
2019-11-12 12:57:23 -0500

    added MOTD

--- api/server.js
+++ api/server.js
@@ -11,7 +11,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   Shoutouts.find()
   .then(shoutouts => {
-    res.status(200).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || "You got this!"
+    res.status(200).json({motd: messageOfTheDay,shoutouts});
   })
   .catch (error => {
     console.error('\nERROR', error);
